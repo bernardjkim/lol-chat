f5608b09151888c7d065537fd74b552d9f8fb0cb
Justin Han <gksksla7140@gmail.com>
2018-12-02 21:23:19 -0800

    server manger done

--- server/server.js
+++ server/server.js
@@ -5,18 +5,7 @@ var port = process.env.PORT || 3001;
 
 const serverManager = require('./serverManager')
 
-io.on("connection", function(client) {
-  console.log("client connected: ", client.id);
-
-  // send a welcome message to the new client
-  const welcome = "Welcome to lol-chat " + client.id;
-  client.emit("message", welcome);
-
-  // broadcast any messages back to all connected clients
-  client.on("message", function(msg) {
-    io.emit("message", msg);
-  });
-});
+io.on('connection', serverManager);
 
 app.listen(port, function() {
   console.log("listening on *:" + port);
