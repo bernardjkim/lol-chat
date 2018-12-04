4931ca5a266deba253bbf534f61ca46a62a7b306
Bernard Kim <bernardkim.seattle@gmail.com>
2018-12-03 19:26:22 -0800

    using a single port

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
 export default function() {
-  const socket = io("http://localhost:3001");
+  const socket = io("http://localhost:3000");
 
   function registerHandler(onMessageReceived) {
     socket.on("message", onMessageReceived);
