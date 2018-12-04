ce181d3e91ee24ea82b52f8195d3b163290988e1
Bernard Kim <bernardkim.seattle@gmail.com>
2018-12-03 19:30:46 -0800

    client connect path

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
 export default function() {
-  const socket = io("http://localhost:3000");
+  const socket = io();
 
   function registerHandler(onMessageReceived) {
     socket.on("message", onMessageReceived);
