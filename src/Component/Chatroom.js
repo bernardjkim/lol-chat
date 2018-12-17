8d66ab4cc3656043e02e969b18c9a583a7bfe437
Bernard Kim <bernardkim.seattle@gmail.com>
2018-12-16 16:36:21 -0800

    ...

--- src/Component/Chatroom.js
+++ src/Component/Chatroom.js
@@ -25,6 +25,20 @@ const mediaStreamConstraints = {
   video: true
 };
 
+var pcConfig = {
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302"
+    }
+  ]
+};
+
+// Set up audio and video regardless of what devices are present.
+var sdpConstraints = {
+  offerToReceiveAudio: true,
+  offerToReceiveVideo: true
+};
+
 class Chatroom extends React.Component {
   constructor(props) {
     super(props);
@@ -37,6 +51,8 @@ class Chatroom extends React.Component {
       msg: "",
       modalOpen: true,
       localStream: false,
+      // NOTE: is pc mutable? is that a problem to store it as a state variable?
+      pc: false,
 
       // TOOD: move to separate file?
       //https://cloud.google.com/translate/docs/languages
@@ -135,7 +151,11 @@ class Chatroom extends React.Component {
   setUsername(username) {
     // connect socket
     var client = socket();
-    client.registerHandler(this.onMessageReceived, this.onMembersReceived);
+    client.registerHandler(
+      this.onMessageReceived,
+      this.onMembersReceived,
+      this.messageHandler
+    );
 
     // set username
     client.setUsername(username);
@@ -154,25 +174,150 @@ class Chatroom extends React.Component {
     this.messagesEnd.scrollIntoView({ behavior: "smooth" });
   };
 
-  getMedia() {
-    // var localStream;
-    // var localVideo = document.querySelector("video");
-    // var remoteVideo = document.querySelector("#remoteVideo");
-
+  getMedia = () => {
     navigator.mediaDevices
       .getUserMedia(mediaStreamConstraints)
       .then(this.gotStream)
       .catch(function(e) {
         alert("getUserMedia() error: " + e);
       });
-  }
+  };
 
   gotStream = stream => {
     console.log("Adding local stream.");
     this.setState({ localStream: stream });
-    // this.video.srcObject = stream;
-    // localVideo.srcObject = stream;
-    // sendMessage("got user media");
+    this.state.client.sendMessage("got user media");
+    this.maybeStart();
+  };
+
+  maybeStart = () => {
+    console.log(">>>>>>> maybeStart() ", this.state.localStream);
+    if (this.state.localStream) {
+      console.log(">>>>>> creating peer connection");
+      this.createPeerConnection();
+
+      var pc = this.state.pc;
+      pc.addStream(this.state.localStream);
+      this.setState({ pc: pc });
+
+      this.doCall();
+    }
+  };
+
+  createPeerConnection = () => {
+    try {
+      var pc = new RTCPeerConnection(null);
+      pc.onicecandidate = this.handleIceCandidate;
+      pc.onaddstream = this.handleRemoteStreamAdded;
+      pc.onremovestream = this.handleRemoteStreamRemoved;
+
+      this.setState({ pc });
+      console.log("Created RTCPeerConnnection");
+    } catch (e) {
+      console.log("Failed to create PeerConnection, exception: " + e.message);
+      alert("Cannot create RTCPeerConnection object.");
+      return;
+    }
+  };
+
+  handleIceCandidate = event => {
+    console.log("icecandidate event: ", event);
+    if (event.candidate) {
+      this.state.client.sendMessage({
+        type: "candidate",
+        label: event.candidate.sdpMLineIndex,
+        id: event.candidate.sdpMid,
+        candidate: event.candidate.candidate
+      });
+    } else {
+      console.log("End of candidates.");
+    }
+  };
+
+  handleRemoteStreamAdded = event => {
+    console.log("Remote stream added.");
+    this.setState({ remoteStream: event.stream });
+    // this.state.remoteStream = event.stream;
+    // remoteVideo.srcObject = remoteStream;
+  };
+
+  handleRemoteStreamRemoved(event) {
+    console.log("Remote stream removed. Event: ", event);
+  }
+
+  handleCreateOfferError(event) {
+    console.log("createOffer() error: ", event);
+  }
+
+  doCall = () => {
+    console.log("Sending offer to peer");
+    this.state.pc.createOffer(
+      this.setLocalAndSendMessage,
+      this.handleCreateOfferError
+    );
+  };
+
+  setLocalAndSendMessage = sessionDescription => {
+    var pc = this.state.pc;
+    pc.setLocalDescription(sessionDescription);
+    this.setState({ pc });
+
+    console.log("setLocalAndSendMessage sending message", sessionDescription);
+    this.state.client.sendMessage(sessionDescription);
+  };
+
+  onCreateSessionDescriptionError(error) {
+    console.log("Failed to create session description: " + error.toString());
+    // trace("Failed to create session description: " + error.toString());
+  }
+
+  doAnswer = () => {
+    console.log("Sending answer to peer.");
+    this.state.pc
+      .createAnswer()
+      .then(this.setLocalAndSendMessage, this.onCreateSessionDescriptionError);
+  };
+
+  handleRemoteHangup = () => {
+    console.log("Session terminated.");
+    this.stop();
+  };
+
+  stop = () => {
+    var pc = this.state.pc;
+    pc.close();
+    pc = null;
+    this.setState({ pc });
+  };
+
+  // This client receives a message
+  messageHandler = message => {
+    console.log("Client received message:", message);
+    var pc;
+    if (message === "got user media") {
+      this.maybeStart();
+    } else if (message.type === "offer") {
+      this.maybeStart();
+
+      pc = this.state.pc;
+      pc.setRemoteDescription(new RTCSessionDescription(message));
+      this.setState({ pc });
+      this.doAnswer();
+    } else if (message.type === "answer") {
+      pc = this.state.pc;
+      pc.setRemoteDescription(new RTCSessionDescription(message));
+      this.setState({ pc });
+    } else if (message.type === "candidate") {
+      var candidate = new RTCIceCandidate({
+        sdpMLineIndex: message.label,
+        candidate: message.candidate
+      });
+      pc = this.state.pc;
+      pc.addIceCandidate(candidate);
+      this.setState({ pc });
+    } else if (message === "bye") {
+      this.handleRemoteHangup();
+    }
   };
 
   render() {
@@ -183,13 +328,20 @@ class Chatroom extends React.Component {
             <video
               autoPlay
               playsInline
-              ref={video => {
-                video.srcObject = this.state.localStream;
+              // ref={video1 => {
+              //   video1.srcObject = this.state.localStream;
+              // }}
+            />
+          )}
+          {this.state.remoteStream && (
+            <video
+              autoPlay
+              playsInline
+              ref={video2 => {
+                video2.srcObject = this.state.remoteStream;
               }}
             />
           )}
-          {/* <video id="localVideo" autoPlay muted playsInline />
-          <video id="remoteVideo" autoPlay playsInline /> */}
         </div>
         <div id="container-left">
           <h3 id="room-name">{this.state.chatroom}</h3>
--- src/socket.js
+++ src/socket.js
@@ -1,36 +1,17 @@
 import io from "socket.io-client";
 
-var isChannelReady = false;
-var isInitiator = false;
-var isStarted = false;
-var localStream;
-var pc;
-var remoteStream;
-var turnReady;
-
-var pcConfig = {
-  iceServers: [
-    {
-      urls: "stun:stun.l.google.com:19302"
-    }
-  ]
-};
-
-// Set up audio and video regardless of what devices are present.
-var sdpConstraints = {
-  offerToReceiveAudio: true,
-  offerToReceiveVideo: true
-};
-
-var localVideo = document.querySelector("#localVideo");
-var remoteVideo = document.querySelector("#remoteVideo");
-
 export default function() {
   const socket = io();
 
-  function registerHandler(onMessageReceived, onMembersReceived) {
-    socket.on("chat-message", onMessageReceived);
+  // TODO: probably want to allow handlers to be registered for any message type we give it
+  function registerHandler(
+    onChatMessageReceived,
+    onMembersReceived,
+    onMessageReceived
+  ) {
+    socket.on("chat-message", onChatMessageReceived);
     socket.on("members", onMembersReceived);
+    socket.on("message", onMessageReceived);
   }
 
   function setLanguage(language) {
@@ -44,6 +25,7 @@ export default function() {
   function unregisterHandler() {
     socket.off("chat-message");
     socket.off("members");
+    socket.off("message");
   }
 
   function message(msg, chatroomName) {
@@ -52,8 +34,6 @@ export default function() {
 
   function joinRoom(chatroomName) {
     socket.emit("join", chatroomName);
-    isChannelReady = true;
-    isInitiator = true;
   }
 
   function sendMessage(message) {
@@ -61,174 +41,9 @@ export default function() {
     socket.emit("message", message);
   }
 
-  // This client receives a message
-  socket.on("message", function(message) {
-    console.log("Client received message:", message);
-    if (message === "got user media") {
-      maybeStart();
-    } else if (message.type === "offer") {
-      if (!isInitiator && !isStarted) {
-        maybeStart();
-      }
-      pc.setRemoteDescription(new RTCSessionDescription(message));
-      doAnswer();
-    } else if (message.type === "answer" && isStarted) {
-      pc.setRemoteDescription(new RTCSessionDescription(message));
-    } else if (message.type === "candidate" && isStarted) {
-      var candidate = new RTCIceCandidate({
-        sdpMLineIndex: message.label,
-        candidate: message.candidate
-      });
-      pc.addIceCandidate(candidate);
-    } else if (message === "bye" && isStarted) {
-      handleRemoteHangup();
-    }
-  });
-
-  //////////////////////////////////////////////////////////////////////////////
-
-  function gotStream(stream) {
-    console.log("Adding local stream.");
-    localStream = stream;
-    localVideo.srcObject = stream;
-    sendMessage("got user media");
-  }
-
-  function maybeStart() {
-    console.log(
-      ">>>>>>> maybeStart() ",
-      isStarted,
-      localStream,
-      isChannelReady
-    );
-    if (!isStarted && typeof localStream !== "undefined" && isChannelReady) {
-      console.log(">>>>>> creating peer connection");
-      createPeerConnection();
-      pc.addStream(localStream);
-      isStarted = true;
-      console.log("isInitiator", isInitiator);
-      if (isInitiator) {
-        doCall();
-      }
-    }
-  }
-
   window.onbeforeunload = function() {
     sendMessage("bye");
   };
-
-  function createPeerConnection() {
-    try {
-      pc = new RTCPeerConnection(null);
-      pc.onicecandidate = handleIceCandidate;
-      pc.onaddstream = handleRemoteStreamAdded;
-      pc.onremovestream = handleRemoteStreamRemoved;
-      console.log("Created RTCPeerConnnection");
-    } catch (e) {
-      console.log("Failed to create PeerConnection, exception: " + e.message);
-      alert("Cannot create RTCPeerConnection object.");
-      return;
-    }
-  }
-
-  function handleIceCandidate(event) {
-    console.log("icecandidate event: ", event);
-    if (event.candidate) {
-      sendMessage({
-        type: "candidate",
-        label: event.candidate.sdpMLineIndex,
-        id: event.candidate.sdpMid,
-        candidate: event.candidate.candidate
-      });
-    } else {
-      console.log("End of candidates.");
-    }
-  }
-
-  function handleCreateOfferError(event) {
-    console.log("createOffer() error: ", event);
-  }
-
-  function doCall() {
-    console.log("Sending offer to peer");
-    pc.createOffer(setLocalAndSendMessage, handleCreateOfferError);
-  }
-
-  function doAnswer() {
-    console.log("Sending answer to peer.");
-    pc.createAnswer().then(
-      setLocalAndSendMessage,
-      onCreateSessionDescriptionError
-    );
-  }
-
-  function setLocalAndSendMessage(sessionDescription) {
-    pc.setLocalDescription(sessionDescription);
-    console.log("setLocalAndSendMessage sending message", sessionDescription);
-    sendMessage(sessionDescription);
-  }
-
-  function onCreateSessionDescriptionError(error) {
-    console.error(error);
-    // trace("Failed to create session description: " + error.toString());
-  }
-
-  function requestTurn(turnURL) {
-    var turnExists = false;
-    for (var i in pcConfig.iceServers) {
-      if (pcConfig.iceServers[i].urls.substr(0, 5) === "turn:") {
-        turnExists = true;
-        turnReady = true;
-        break;
-      }
-    }
-    if (!turnExists) {
-      console.log("Getting TURN server from ", turnURL);
-      // No TURN server. Get one from computeengineondemand.appspot.com:
-      var xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          var turnServer = JSON.parse(xhr.responseText);
-          console.log("Got TURN server: ", turnServer);
-          pcConfig.iceServers.push({
-            urls: "turn:" + turnServer.username + "@" + turnServer.turn,
-            credential: turnServer.password
-          });
-          turnReady = true;
-        }
-      };
-      xhr.open("GET", turnURL, true);
-      xhr.send();
-    }
-  }
-
-  function handleRemoteStreamAdded(event) {
-    console.log("Remote stream added.");
-    remoteStream = event.stream;
-    remoteVideo.srcObject = remoteStream;
-  }
-
-  function handleRemoteStreamRemoved(event) {
-    console.log("Remote stream removed. Event: ", event);
-  }
-
-  function hangup() {
-    console.log("Hanging up.");
-    stop();
-    sendMessage("bye");
-  }
-
-  function handleRemoteHangup() {
-    console.log("Session terminated.");
-    stop();
-    isInitiator = false;
-  }
-
-  function stop() {
-    isStarted = false;
-    pc.close();
-    pc = null;
-  }
 
   return {
     registerHandler,
