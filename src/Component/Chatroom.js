4e89ad055881d19319f4e8d8a995378d4fd7da8a
Bernard Kim <bernardkim.seattle@gmail.com>
2018-12-17 17:37:14 -0800

    turn server?

--- src/Component/Chatroom.js
+++ src/Component/Chatroom.js
@@ -29,9 +29,28 @@ var pcConfig = {
   iceServers: [
     {
       urls: "stun:stun.l.google.com:19302"
+    },
+    {
+      urls: [
+        "turn:webrtcweb.com:7788", // coTURN 7788+8877
+        "turn:webrtcweb.com:4455?transport=udp", // restund udp
+
+        "turn:webrtcweb.com:8877?transport=udp", // coTURN udp
+        "turn:webrtcweb.com:8877?transport=tcp" // coTURN tcp
+      ],
+      username: "muazkh",
+      credential: "muazkh"
     }
   ]
 };
+
+// var pcConfig = {
+//   iceServers: [
+//     {
+//       urls: "stun:stun.l.google.com:19302"
+//     }
+//   ]
+// };
 
 // Set up audio and video regardless of what devices are present.
 var sdpConstraints = {
