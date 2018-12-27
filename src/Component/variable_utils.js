a67428265d805a848069a78be46fc70a35c2964b
Bernard Kim <bernardkim.seattle@gmail.com>
2018-12-27 03:02:48 -0800

    turn server

--- src/Component/variable_utils.js
+++ src/Component/variable_utils.js
@@ -1,21 +1,20 @@
 module.exports = {
-
   pcConfig: {
     iceServers: [
       {
         urls: "stun:stun.l.google.com:19302"
-      }
-      // {
-      //   urls: [
-      //     "turn:webrtcweb.com:7788", // coTURN 7788+8877
-      //     "turn:webrtcweb.com:4455?transport=udp", // restund udp
+      },
+      {
+        urls: [
+          "turn:webrtcweb.com:7788", // coTURN 7788+8877
+          "turn:webrtcweb.com:4455?transport=udp", // restund udp
 
-      //     "turn:webrtcweb.com:8877?transport=udp", // coTURN udp
-      //     "turn:webrtcweb.com:8877?transport=tcp" // coTURN tcp
-      //   ],
-      //   username: "muazkh",
-      //   credential: "muazkh"
-      // }
+          "turn:webrtcweb.com:8877?transport=udp", // coTURN udp
+          "turn:webrtcweb.com:8877?transport=tcp" // coTURN tcp
+        ],
+        username: "muazkh",
+        credential: "muazkh"
+      }
     ]
   },
   // Set up audio and video regardless of what devices are present.
