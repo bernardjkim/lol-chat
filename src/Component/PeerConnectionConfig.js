ae435b3c3f8f1725e261aafc2730c2032f93669c
Bernard Kim <bernardkim.seattle@gmail.com>
2018-12-28 00:47:54 -0800

    client side env variables

--- src/Component/PeerConnectionConfig.js
+++ src/Component/PeerConnectionConfig.js
@@ -6,10 +6,10 @@
 export default {
   iceServers: [
     {
-      urls: process.env.STUN_SERVER
+      urls: [process.env.STUN_SERVER]
     },
     {
-      urls: process.env.TURN_SERVER,
+      urls: [process.env.TURN_SERVER],
       username: process.env.TURN_USERNAME,
       credential: process.env.TURN_PASSWORD
     }
